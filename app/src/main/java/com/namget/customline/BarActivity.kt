f8cb3439b6055db10acb91d4b579bf06e84083a5
leejaewoo <yeboykr@naver.com>
2019-05-08 16:55:52 +0900

    - bar 만들어달라는 요청 들어오면 만들어야함

---
+++ app/src/main/java/com/namget/customline/BarActivity.kt
@@ -0,0 +1,15 @@
+package com.namget.customline
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BarActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bar)
+
+
+    }
+}
